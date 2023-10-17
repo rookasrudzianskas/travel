13167237d823060c21ef8e9aef0bd52e6b0aafe9
Rokas <rookas.rudzenskas@gmail.com>
2023-10-17 10:51:37 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@
 // import Features from "@/components/Features";
 // import GetApp from "@/components/GetApp";
 // import Guide from "@/components/Guide";
-// import Hero from "@/components/Hero";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
-      {/*<Hero />*/}
+      <Hero />
       {/*<Camp />*/}
       {/*<Guide />*/}
       {/*<Features />*/}
