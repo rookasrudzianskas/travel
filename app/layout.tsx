f53bac155db58a3fcc427572690621e5b321f22c
Rokas <rookas.rudzenskas@gmail.com>
2023-10-17 09:25:40 +0300

    added button fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 import './globals.css'
-// import Navbar from '@/components/Navbar';
+import Navbar from '@/components/Navbar';
 // import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/*<Navbar />*/}
+        <Navbar />
           <main className="relative overflow-hidden">
             {children}
           </main>
