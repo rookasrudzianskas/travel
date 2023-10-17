b3d19ce4937c6628388bd97b9385bdea100e9ded
Rokas <rookas.rudzenskas@gmail.com>
2023-10-17 11:02:31 +0300

    camp added

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-// import Camp from "@/components/Camp";
+import Camp from "@/components/Camp";
 // import Features from "@/components/Features";
 // import GetApp from "@/components/GetApp";
-// import Guide from "@/components/Guide";
+import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      {/*<Camp />*/}
-      {/*<Guide />*/}
+      <Camp />
+      <Guide />
       {/*<Features />*/}
       {/*<GetApp />*/}
     </>
